bdeb7f3050c439c53f0c8bea26dd73721728285c
ikimodus <ifhumail@gmail.com>
2026-04-26 01:40:06 +0200

    Foundations Ex.8 me overcomplicating this

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+let targetArr = [1, 2, 2, 4]
+
+const removeFromArray = function(arr, ...target) {
+
+    for (targetIndex = 0; targetIndex < target.length; targetIndex++) {
+        for (arrIndex = 0; arrIndex < arr.length; arrIndex++) {
+            if (targetIndex === arrIndex) arr.splice(arr[arrIndex], 1)
+        }
+    }
+
+    return arr
 };
+
+/*        arr.splice(arr.indexOf(target[i]), 1) */
+console.log(removeFromArray(targetArr, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;

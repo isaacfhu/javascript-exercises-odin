680d9683b899e742def7c35a0a42dc46fb87b92d
ikimodus <ifhumail@gmail.com>
2026-04-25 14:45:51 +0200

    Ex. 6 Overcomplicated compared to the solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, repeatAmount) {
+    if (repeatAmount < 0) {
+        return "ERROR"
+    }
 
+    let arr = []
+    for (i = 0; i < repeatAmount; i++) {
+        arr.push(str)
+    }
+    return arr.join('')
 };
+
+console.log(repeatString('hey', 1.5))
 
 // Do not edit below this line
 module.exports = repeatString;

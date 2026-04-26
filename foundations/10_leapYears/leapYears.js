f419af0aa5ac7527f533773f82d10264708d7ddd
ikimodus <ifhumail@gmail.com>
2026-04-26 18:58:23 +0200

    Ex. 10 Foundations -Use of a little help but not much at all- Basically followed what was written and converted into code

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    console.log(year / 4)
+    if (Number.isInteger(year / 4) &&
+        !Number.isInteger(year / 100)
+    ) 
+        return true
+    else if (Number.isInteger(year / 400))
+        return true
+    else 
+        return false
 };
+
+console.log(leapYears(1600))
+
+/*    if (Number.isInteger(year / 4) && 
+        !Number.isInteger(year / 100) &&
+        Number.isInteger(year / 400)
+    ) 
+        return true
+    else 
+        return false*/
 
 // Do not edit below this line
 module.exports = leapYears;

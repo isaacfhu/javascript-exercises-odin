0f7ca6f15827ed9663f9811878ebd48f031b6e0e
ikimodus <ifhumail@gmail.com>
2026-06-08 19:53:02 +0200

    13-palindromes Did not use array methods at first, used string methods instead All went smooth sailing after using array methods

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strTable = str.toLowerCase().split("");
 
+  const filteredTable = strTable.filter(
+    (item) => item !== "," && item !== "." && item !== "!" && item !== " ",
+  );
+  const newStr = filteredTable.join("");
+  const reversedStr = filteredTable.reverse().join("");
+
+  console.log(newStr, "|||");
+  return reversedStr === newStr;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;

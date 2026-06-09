1704d3846b2cb073c5c5c6b5ea82996d31a808f4
ikimodus <ifhumail@gmail.com>
2026-06-09 03:48:49 +0200

    14-fibonacci My answer used arrays whereas solution didn't. I did need a small bit of help at the end.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
 
+  const newNum = Number(num) - 1;
+  let fArray = [1, 1, 2];
+  let initLength = fArray.length;
+
+  fArray[-1] = 0;
+  for (let i = initLength; i <= newNum; i++) {
+    fArray[i] = fArray[i - 1] + fArray[i - 2];
+  }
+  return fArray[newNum];
 };
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;

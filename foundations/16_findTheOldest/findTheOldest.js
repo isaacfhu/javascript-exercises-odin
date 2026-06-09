a9769d7407d4319058de76dc68b482eb720ee5ca
ikimodus <ifhumail@gmail.com>
2026-06-09 04:19:23 +0200

    16-findTheOldest Solved smoothly. I'm starting to get the hang of this

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let newArr = arr.sort((previous, current) => {
+    if (current.yearOfDeath === undefined) {
+      current.yearOfDeath = new Date().getFullYear();
+    }
+    return (
+      current.yearOfDeath -
+      current.yearOfBirth -
+      (previous.yearOfDeath - previous.yearOfBirth)
+    );
+  });
+  return newArr[0];
 };
 
 // Do not edit below this line
